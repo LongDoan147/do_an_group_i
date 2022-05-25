cc0eb471e74806439fc0cdd0817bc1b7ddf4d1b3
LongDoan <18211tt2257@mail.tdc.edu.vn>
2022-05-25 13:47:09 +0700

    Add module 7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/module1.css')}}">
+    <link rel="stylesheet" href="{{asset('css/module7.css')}}">
     <script src="https://kit.fontawesome.com/72db99a8b6.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>

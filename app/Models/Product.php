b1ab1d7f65173fcbb34df39a1a2dbb796e179895
ngocyen02 <ngocyentxrt021201@gmailcom>
2022-06-10 17:00:12 +0700

    edit file product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
     protected $table = 'tbl_product';
     protected $primaryKey = 'product_id';
     protected $fillable =['product_name','category_id','product_desc','product_content',
@@ -16,3 +25,7 @@ class Product extends Model
     }
     use HasFactory;
 }
+
+
+
+

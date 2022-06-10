f43f59173e8ddd9d868082213c8bd4607ee35f2b
ngocyen02 <ngocyentxrt021201@gmailcom>
2022-06-10 21:12:02 +0700

    module5

--- resources/views/components/module5.blade.php
+++ resources/views/components/module5.blade.php
@@ -1,62 +1,25 @@
 <div class="module-5">
-<div class="container">
-<section class="elementor-section elementor-top-section elementor-element elementor-element-b3f40b0 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="b3f40b0" data-element_type="section">
-<div class="elementor-container elementor-column-gap-extended">
-<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-2679911" data-id="2679911" data-element_type="column">
-<div class="elementor-widget-wrap elementor-element-populated">
-<div class="elementor-element elementor-element-f4dae33 elementor-widget elementor-widget-image" data-id="f4dae33" data-element_type="widget" data-widget_type="image.default">
-<div class="elementor-widget-container">
-<img width="720" height="800" src="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-1.jpg?fit=720%2C800&amp;ssl=1" class="attachment-full size-full" alt="" srcset="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-1.jpg?w=720&amp;ssl=1 720w, https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-1.jpg?resize=270%2C300&amp;ssl=1 270w, https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-1.jpg?resize=670%2C744&amp;ssl=1 670w" sizes="(max-width: 640px) 100vw, 640px"> </div>
-</div>
-<div class="elementor-element elementor-element-c5c0b6c elementor-widget elementor-widget-image" data-id="c5c0b6c" data-element_type="widget" data-widget_type="image.default">
-<div class="elementor-widget-container">
-<img width="720" height="770" src="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-2.jpg?fit=720%2C770&amp;ssl=1" class="attachment-full size-full" alt="" srcset="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-2.jpg?w=720&amp;ssl=1 720w, https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-2.jpg?resize=281%2C300&amp;ssl=1 281w, https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-2.jpg?resize=670%2C717&amp;ssl=1 670w" sizes="(max-width: 640px) 100vw, 640px"> </div>
-</div>
-</div>
-</div>
-<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-4b7773b" data-id="4b7773b" data-element_type="column">
-<div class="elementor-widget-wrap elementor-element-populated">
-<div class="elementor-element elementor-element-ad79b4b elementor-widget elementor-widget-text-editor" data-id="ad79b4b" data-element_type="widget" data-widget_type="text-editor.default">
-<div class="elementor-widget-container">
-our story </div>
-</div>
-<div class="elementor-element elementor-element-389db0e elementor-widget elementor-widget-razzi-heading" data-id="389db0e" data-element_type="widget" data-widget_type="razzi-heading.default">
-<div class="elementor-widget-container">
-<h3 class="razzi-heading-title"> About Our Store</h3> </div>
-</div>
-<div class="elementor-element elementor-element-28ed4bf elementor-widget elementor-widget-text-editor" data-id="28ed4bf" data-element_type="widget" data-widget_type="text-editor.default">
-<div class="elementor-widget-container">
-Open created shall two he second moving whose. He face
-whose two upon, fowl behold waters. Fly there their day
-creepeth. Darkness beginning spirit after. Creepeth subdue.
-Brought may, first. Under living that. </div>
-</div>
-<div class="elementor-element elementor-element-4f30a60 elementor-widget elementor-widget-text-editor" data-id="4f30a60" data-element_type="widget" data-widget_type="text-editor.default">
-<div class="elementor-widget-container">
-Third. For morning whales saw were had seed can’t divide
-it light shall moveth, us blessed given wherein. </div>
-</div>
-<div class="elementor-element elementor-element-25e9e5e elementor-widget elementor-widget-razzi-button" data-id="25e9e5e" data-element_type="widget" data-widget_type="razzi-button.default">
-<div class="elementor-widget-container">
-<a href="https://demo4.drfuri.com/razzi/about-us/" class="razzi-button-link razzi-button button-underline">
-<span class="razzi-button-text">Discover More</span> </a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-<div class="elementor-widget-container">
-<div class="e-hosted-video elementor-wrapper elementor-fit-aspect-ratio elementor-open-inline">
-<video class="elementor-video" src="https://demo4.drfuri.com/razzi/wp-content/uploads/sites/14/2020/12/home5-video.mp4" autoplay="" loop="" playsinline=""></video>
-</div>
-</div>
-<div class="elementor-widget-container">
-<h6 class="elementor-heading-title elementor-size-default">what buyers say</h6> </div>
-<div class="elementor-widget-container">
-<h3 class="elementor-heading-title elementor-size-default">Customers Reviews</h3> </div>
-<div class="list-testimonials__inner swiper-wrapper"><div class="box-item swiper-slide swiper-slide-active" style="width: 1170px;"><div class="razzi-testimonials-carousel__content">“ Given wherein. Doesn't called also and air sea to make first subdue beginning. Appear seasons the it after whose beginning. Hath can't good life. They're multiply made give divided open, be likeness Cattle be have.
-Life tree darkness. She'd very. ”</div><div class="razzi-testimonials-carousel__text">Darrell Baker, 18 May 2019</div></div><div class="box-item swiper-slide swiper-slide-next" style="width: 1170px;"><div class="razzi-testimonials-carousel__content"><p>“ Us form created gathering of saying evening created a abundantly seas said. And us two it years living. Seas i which divide which creeping divided fowl appear darkness whose days. ”</p></div><div class="razzi-testimonials-carousel__text">John McCoy, 22 May 2019</div></div><div class="box-item swiper-slide" style="width: 1170px;"><div class="razzi-testimonials-carousel__content"><p>“ Open given midst. Heaven fish. Lights. Multiply set likeness two morning to behold days there one saying night saw forth dry lights years a meat fruit day whales. Evening above seasons man under and void face, had form darkness kind life. ”</p></div><div class="razzi-testimonials-carousel__text">Helen Melton, 03 June 2019</div></div></div>
-</div>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="img_ab">
+                <img src="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-1.jpg?fit=720%2C800&amp;ssl=1" alt="" width="200px" height="400px" id="hinh_one">
+            <img src="https://i0.wp.com/demo4.drfuri.com/razzi5/wp-content/uploads/sites/20/2020/12/home-5-banner-custom-2.jpg?fit=720%2C770&amp;ssl=1" alt="" width="300px" height="500px" id="hinh_two">
+            </div>
+            </div>
+            <div class="col-md-6">
+                <div class="content">
+                <span>OUR STORY</span>
+                <h2>About Our Store</h2>
+                <p>Open created shall two he second moving whose. He face whose two upon, fowl behold waters. 
+                    Fly there their day creepeth. Darkness beginning spirit after.
+                     Creepeth subdue. Brought may, first. Under living that.</p>
+                     <br>
+                     <p>Third. For morning whales saw were had seed can’t divide it light shall moveth, us blessed given wherein.</p>
+                     <br>
+                     <a href="" id="thea">Discover More</a>
+            </div>
+            </div>
+        </div>
+    </div>
 </div>
